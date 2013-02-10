3ae9fde4f3bd795ab7cc69c55bca149b49199cbb
gunnarjv <gunnarjv@gmail.com>
2013-02-10 18:23:19 +0000

    bfs maybe ready

--- src/BFS.java
+++ src/BFS.java
@@ -6,8 +6,6 @@ public class BFS implements Search
 
     public Stack<String> search(Environment env, State state)
     {
-       Stack<String> strat = new Stack<String>();
-
        Node root = new Node(state, null, null);
 
        // If the initial state is goal we are done.
@@ -19,16 +17,17 @@ public class BFS implements Search
        		Node n = f.pop();
        		State s = n.state;
 
-       		for (String c: s.get_legal_moves())
+       		for (String m: s.get_legal_moves())
        		{
-       			Node child = new Node(s.next_state(c), n, c);
+       			Node child = new Node(s.next_state(m), n, m);
        			
-
-
-       			f.add(c);       			
+       			if(is_goal(child.state))
+       				return path(child.state);
+       			else
+	       			f.add(c);       			
        		}
        }
-       return strategy;
+       // We should never get here.
        System.out.println("Something has gone awry! The search returned no solution!");
        System.out.println("Exiting.");
        System.exit();
@@ -39,6 +38,22 @@ public class BFS implements Search
        if(state.dirts.isEmpty() && env.at_home(state.location))
        		return true;
        return false;
+    }
+
+    private Stack<string> path(Node goal)
+    {
+       Stack<String> strat = new Stack<String>();
+
+       strat.push(goal.move);
+       next_state = goal.parent;
+
+       while(next_state)
+       {
+       		strat.push(next_state.goal);
+       		next_state = next_state.parent
+       }
+
+       return strat;
     }
 
 }
