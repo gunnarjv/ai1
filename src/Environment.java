21b042167aeea35f7a70cad040253a6daf9162c4
OpenShift guest <bjarnibenedikts@gmail.com>
2013-02-10 00:20:54 -0500

    bla

---
+++ src/Environment.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Environment{
+    
+    public List<Point2D> obstacles;
+    
+    // r is rows, c i columns
+    public int r, c;
+    
+    // Home
+    public Point2D home;
+    {
+        return home.equals(location);
+    }
+}
---
+++ src/State.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class State {
+    public State(boolean ON, Point2D location, int direction, List<Point2D> dirts)
+    {
+        this.ON = ON;
+        this.location = location;
+        this.direction = direction;
+        this.dirts = dirts;
+    }
+    
+    public boolean ON = false;
+    //Current coordinates
+    public Point2D location;
+    // 1 is North. We will use modular arithmetic for directions.
+    public int direction;
+    //List of dirt coordinates
+    public List<Point2D> dirts;
+    
+    /**************Functions***********************/
+    
+    public List<String> get_legal_moves(Environment env)
+    {
+        List<String> moves = new ArrayList<String>;
+        
+        if(!ON)
+        {
+            moves.add("TURN_ON");
+            return moves;
+        }
+        
+        // Suck, turning is always legal.
+        moves.add("SUCK");
+        moves.add("TURN_RIGHT");
+        moves.add("TURN_LEFT");
+        // We know we must be ON.
+        moves.add("TURN_OFF")
+        
+        //If Go is inside of boundaries and not facing an obstacle add GO                
+        switch(direction)
+        {
+            case 1: // North
+                //If we are at the northmost location
+                if(location.y() == env.c)
+                {
+                    //Create new point with yCord+1 and checks if there is an obstacle with that coordinate
+                    if(env.obstacles.contains(location.x(), new Point(location.y()+1)))
+                        break;
+                }
+                else
+                    moves.add("GO");
+            break;
+            case 2: // East
+            break;
+            case 3: // South
+            break;
+            case 4: // West
+            break;
+        }
+                    
+        return moves;
+    }
+    
+    public State next_state(String move)
+    {
+        
+    }
+}
