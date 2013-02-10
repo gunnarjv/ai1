8ed3c1403b7f86be7acc7a8f21a0e3b8cba6612d
gunnarjv <gunnarjv@gmail.com>
2013-02-10 16:46:53 +0000

    finished interface for search

--- src/BFS.java
+++ src/BFS.java
@@ -2,7 +2,7 @@ import java.util.*
 
 public class BFS implements Search
 {
-    public Stack<String> search()
+    public Stack<String> search(Environment env, State state)
     {
         return new Stack<String>;
     }
--- src/Search.java
+++ src/Search.java
@@ -2,5 +2,5 @@ import java.util.*
 
 public interface Search
 {
-	public Stack<String> search();
+	public Stack<String> search(Environment env, State state);
 }
